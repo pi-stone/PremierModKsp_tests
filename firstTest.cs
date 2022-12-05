50544feea3be58ca5547a4fbd355591259c53068
Utilisateur <Utilisateur@DESKTOP-REPG25O>
2022-12-05 19:57:23 +0100

    Ajoutez des fichiers projet.

---
+++ firstTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace PremierModKSP
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class firstTest : MonoBehaviour
+    {
+        public void Update()
+        {
+            bool key = Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.Alpha1);
+            if (key) 
+            {
+                List<Part> parts = FlightGlobals.ActiveVessel.parts;
+                int index;
+                System.Random rnd = new System.Random();
+                index = rnd.Next(1, parts.Count);
+                parts[index].explode();
+            }
+        }
+
+    }
+}
