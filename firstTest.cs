b6186157aeac30a7606a1aed53890dec583a10e1
mani gillier <manigillier@gmail.com>
2022-12-06 18:30:43 +0100

    Made texture work but not load // TODO Made tests about clicks

--- firstTest.cs
+++ firstTest.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
+using KSP.UI.Screens;
 
 
 namespace PremierModKSP
@@ -11,18 +14,34 @@ namespace PremierModKSP
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     public class firstTest : MonoBehaviour
     {
-        public void Update()
+        private const string Path = "D:/jeux/steam/steamapps/common/Kerbal Space Program/modding/PremierModKSP/PremierModKSP/";
+        private const string Name = "MJ2.png";
+
+        ApplicationLauncher applicationLauncher;
+        ApplicationLauncherButton applicationLauncherButton;
+        public void Start()
         {
-            bool key = Input.GetKey(KeyCode.LeftAlt) && Input.GetKey(KeyCode.Alpha1);
-            if (key) 
-            {
-                List<Part> parts = FlightGlobals.ActiveVessel.parts;
-                int index;
-                System.Random rnd = new System.Random();
-                index = rnd.Next(1, parts.Count);
-                parts[index].explode();
-            }
+            Debug.Log(ApplicationLauncher.Instance);
+            applicationLauncher = ApplicationLauncher.Instance;
+            Texture2D tex = new Texture2D(38, 38);
+
+            applicationLauncherButton = applicationLauncher.AddModApplication(OnTrue,
+            OnFalse,
+            null,
+            null,
+            null,
+            null,
+            ApplicationLauncher.AppScenes.FLIGHT,
+            tex);
         }
 
+        public void OnTrue()
+        {
+            Debug.Log("Oppened");
+        }
+        public void OnFalse()
+        {
+            Debug.Log("Closed");
+        }
     }
 }
